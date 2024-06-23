244148fb315f38711509314d3ae932e2673b6bd0
HeXiaoPi <hechangjie0619@icloud.com>
2024-06-23 21:56:35 +0800

    feat: 增加database-sql

--- go.mod
+++ go.mod
@@ -4,8 +4,12 @@ go 1.20
 
 require (
 	github.com/garyburd/redigo v1.6.3
+	github.com/go-sql-driver/mysql v1.8.1
 	github.com/spf13/pflag v1.0.5
 	go.uber.org/ratelimit v0.2.0
 )
 
-require github.com/andres-erbsen/clock v0.0.0-20160526145045-9e14626cd129 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/andres-erbsen/clock v0.0.0-20160526145045-9e14626cd129 // indirect
+)
---
+++ standard-library/database-sql/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:930619@/information_schema")
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	if err := db.Ping(); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%+v", db.Stats())
+}
